30cb78b15b9bc2e04e58b0c515386f87d813ef55
kazushi-F <53499872+kazushi-F@users.noreply.github.com>
2020-07-18 17:56:12 +0900

    可視性の向上

--- script.js
+++ script.js
@@ -1,140 +1,148 @@
-var or = 0;
-var count;
-var countTrue;
+var enterMove = 0;
+var trueAnswer = "";
+var answerCount;
+var trueCount;
 var before;
 var after;
 var level;
-var timeCount;
-var timer1;
+var countTime;
+var timer;
+
 function start(){
-    let countTime = parseInt(document.getElementById("time").value);
-    count = 0;
-    countTrue = 0;
-    before = document.getElementById("before").value;
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("allCount").style.display = "none";
+    document.getElementById("trueCount").style.display = "none";
+    document.getElementById("percent").style.display = "none";
+    document.getElementById("countTime").style.display = "block";
+    answerCount = 0;
+    trueCount = 0;
+    before = parseInt(document.getElementById("before").value);
     after = parseInt(document.getElementById("after").value);
     level = parseInt(document.getElementById("level").value);
-    timeCount = countTime * 60;
-    document.getElementById("timeCount").innerText = "残り時間：　" + countTime + "分0秒";
-    timeCount--;
-    timer1 = setInterval("countdown()", 1000);
-    question();
+    countTime = parseInt(document.getElementById("time").value);
+    document.getElementById("countTime").innerText = "残り時間：　" + countTime + "分0秒";
+    countTime = countTime * 60;
+    countTime--;
+    timer = setInterval("countdown()", 1000);
     if(after == 2){
         document.getElementById("answer").maxLength = level;
     }else if(after == 16){
         document.getElementById("answer").maxLength = Math.floor((level - 1) / 4) + 1;
     }
-    document.getElementById("queButton").disabled = true;
-    document.getElementById("countAll").style.display = "none";
-    document.getElementById("countTrue").style.display = "none";
-    document.getElementById("countPar").style.display = "none";
+    question();
 }
+
 function countdown(){
-    let sec = timeCount % 60;
-    let min = (timeCount - sec) / 60;
-    document.getElementById("timeCount").innerText = "残り時間：　" + min + "分" + sec + "秒";
-    if(timeCount == 0){
-        clearInterval(timer1);
-        let par = Math.round((countTrue / count) * 1000) / 10;
-        document.getElementById("test").style.display = "none";
+    let minute = Math.floor(countTime / 60);
+    let second = countTime % 60;
+    document.getElementById("countTime").innerText = "残り時間：　" + minute + "分" + second + "秒";
+    if(countTime == 0){
+        clearInterval(timer);
+        let percent = Math.round((trueCount / answerCount) * 1000) / 10;
+        document.getElementById("question").style.display = "none";
         document.getElementById("answerPar").style.display = "none";
-        document.getElementById("countAll").style.display = "block";
-        document.getElementById("countTrue").style.display = "block";
-        document.getElementById("countPar").style.display = "block";
         document.getElementById("judge").style.display = "none";
-        document.getElementById("countAll").innerText = "回答数：　" + count;
-        document.getElementById("countTrue").innerText = "正解数：　" + countTrue;
-        document.getElementById("countPar").innerText = "正解率：　" + par + "％";
-        document.getElementById("queButton").disabled = false;
+        document.getElementById("allCount").style.display = "block";
+        document.getElementById("trueCount").style.display = "block";
+        document.getElementById("percent").style.display = "block";
+        document.getElementById("allCount").innerText = "回答数：　" + answerCount;
+        document.getElementById("trueCount").innerText = "正解数：　" + trueCount;
+        document.getElementById("percent").innerText = "正解率：　" + percent + "％";
+        document.getElementById("startButton").disabled = false;
     }
-    timeCount--;
+    countTime--;
 }
+
+function question(){
+    enterMove = 0;
+    let question = "";
+    if(before == 2){
+        question = create2();
+    }else if(before == 10){
+        question = create10();
+    }else{
+        question = create16();
+    }
+    document.getElementById("question").style.display = "block";
+    document.getElementById("answerPar").style.display = "block";
+    document.getElementById("question").innerText = "問題　" + question;
+    document.getElementById("answer").value = "";
+    if(after == 2){
+        if(before == 16){
+            trueAnswer = parseInt(question, 16).toString(2);
+        }else{
+            trueAnswer = question.toString(2);
+        }
+        while(trueAnswer.length != level){
+            trueAnswer = "0" + trueAnswer;
+        }
+    }else if(after == 10){
+        trueAnswer = String(parseInt(question, before));
+    }else{
+        if(before == 2){
+            trueAnswer = parseInt(question, 2).toString(16);
+        }else{
+            trueAnswer = question.toString(16);
+        }
+        while(trueAnswer.length != (Math.floor((level - 1) / 4) + 1)){
+            trueAnswer = "0" + trueAnswer;
+        }
+    }
+    document.getElementById("judge").innerText = trueAnswer;
+    document.getElementById("judge").style.display = "none";
+}
+
+function create2(){
+    let question = "";
+    for(let i = 0; i < level; i++){
+        question = question + Math.floor(Math.random() * 2);
+    }
+    return question;
+}
+
+function create10(){
+    let max = Math.pow(2, level);
+    let question = "";
+    question = Math.floor(Math.random() * max);
+    return question;
+}
+
+function create16(){
+    let digit = Math.floor((level - 1) / 4) + 1;
+    let question = "";
+    question = create10().toString(16);
+    while(question.length != digit){
+        question = "0" + question;
+    }
+    return question;
+}
+
 function answer(){
-    count++;
-    or = 1;
+    enterMove = 1;
+    answerCount++;
     let answer = String(document.getElementById("answer").value).toLowerCase();
-    let trueAns = document.getElementById("judge").innerText;
     let judge = "";
-    while(answer.length < trueAns.length){
+    while(answer.length < trueAnswer.length){
         answer = "0" + answer;
     }
-    if(answer == trueAns){
+    if(answer == trueAnswer){
         judge = "正解〇";
-        countTrue++;
+        trueCount++;
     }else{
-        judge = "不正解☓　（正答）" + trueAns;
+        judge = "不正解☓　（正答）" + trueAnswer;
     }
     document.getElementById("judge").innerText = judge;
     document.getElementById("judge").style.display = "block";
 }
-function question(){
-    or = 0;
-    let test = "";
-    let trueAns = "";
-    if(before == 2){
-        test = create2();
-    }else if(before == 10){
-        test = create10();
-    }else{
-        test = create16();
-    }
-    document.getElementById("test").innerText = "問題　" + test;
-    document.getElementById("test").style.display = "block";
-    document.getElementById("answer").value = "";
-    document.getElementById("answerPar").style.display = "block";
-    if(after == 2){
-        if(before == 16){
-            trueAns = parseInt(test, 16).toString(2);
-        }else{
-            trueAns = test.toString(2);
-        }
-        while(trueAns.length != level){
-            trueAns = "0" + trueAns;
-        }
-    }else if(after == 10){
-        trueAns = String(parseInt(test, before));
-    }else{
-        if(before == 2){
-            trueAns = parseInt(test, 2).toString(16);
-        }else{
-            trueAns = test.toString(16);
-        }
-        while(trueAns.length != (Math.floor((level - 1) / 4) + 1)){
-            trueAns = "0" + trueAns;
-        }
-    }
-    document.getElementById("judge").innerText = trueAns;
-    document.getElementById("judge").style.display = "none";
-}
-function create2(){
-    let num = "";
-    for(let i = 0; i < level; i++){
-        num = num + Math.floor(Math.random() * 2);
-    }
-    return num;
-}
-function create10(){
-    let max = Math.pow(2, level);
-    let num = "";
-    num = Math.floor(Math.random() * max);
-    return num;
-}
-function create16(){
-    let digit = Math.floor((level - 1) / 4) + 1;
-    let num = "";
-    num = create10().toString(16);
-    while(num.length != digit){
-        num = "0" + num;
-    }
-    return num;
-}
+
 function keydownEnter(){
     if(window.event.keyCode == 13){
-        if(or){
-            or = 0;
+        if(enterMove){
+            enterMove = 0;
             question();
         }else{
-            or = 1;
-            document.getElementById("ansButton").click();
+            enterMove = 1;
+            document.getElementById("answerButton").click();
         }
     }
 }
